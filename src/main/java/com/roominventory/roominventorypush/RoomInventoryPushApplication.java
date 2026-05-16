8cc477dab5eb1f7ae01ebdab6e77b144f59696e5
Lickshaan.K <lickshaan420@gmail.com>
2026-05-16 23:28:00 +0530

    init commit

---
+++ src/main/java/com/roominventory/roominventorypush/RoomInventoryPushApplication.java
@@ -0,0 +1,13 @@
+package com.roominventory.roominventorypush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomInventoryPushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomInventoryPushApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/roominventory/roominventorypush/RoomInventoryPushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roominventory.roominventorypush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomInventoryPushApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
